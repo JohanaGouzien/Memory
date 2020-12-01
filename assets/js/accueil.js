cfaa5121e59a603666e8d7ab2079f3a98ddfc5c2
gouzien <johana.gouzien+lamanu@gmail.com>
2020-12-01 11:46:45 +0100

    page d'acuueil

---
+++ assets/js/accueil.js
@@ -0,0 +1,17 @@
+//function bouton play
+$(document).ready(function() {
+
+
+$(".contenu").hide();
+
+$(".btnplay").click(function() { 
+
+    $(".fond").hide("slide", {
+        direction: "up"
+    }, 1200, function() {
+        $(".contenu").show("slide", 1000);
+       
+
+    });
+});
+});
